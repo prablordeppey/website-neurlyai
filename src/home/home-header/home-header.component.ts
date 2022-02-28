5392bf955d3592029ffade2be942e3c9cdcefbe1
icetee4eva <30740369+icetee4eva@users.noreply.github.com>
2022-02-28 18:43:27 +0000

    carousal for external links and new products

--- src/home/home-header/home-header.component.ts
+++ src/home/home-header/home-header.component.ts
@@ -8,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class HomeHeaderComponent implements OnInit {
 
   title = 'NeurlyAI';
+  redirect_links = {
+    our_email: 'mailto:prablordeppey@gmail.com',
+    website_repo: 'https://github.com/prablordeppey',
+    youtube_channel: 'https://www.youtube.com/channel/UCFlgJ0SvO5ZWWMJB657_Qdw',
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  redirectTo(destination: string) {
+    const url = (<any>this).redirect_links[destination];
+    window.open(url, '_blank');
+  }
 }
